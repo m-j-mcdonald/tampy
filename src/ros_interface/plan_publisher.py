4ff1ea2f4ff22cec3449ae8fa795dcf12a421ca0
Michael McDonald <m_j_mcdonald@comcast.net>
2016-11-15 19:02:14 -0800

    Fixed finding failed predicates

--- src/ros_interface/plan_publisher.py
+++ src/ros_interface/plan_publisher.py
@@ -4,6 +4,7 @@ import rospy
 from std_msgs.msg import String
 
 from baxter_plan.msg import ActionMSG
+from baxter_plan.msg import ActionPredMSG
 from baxter_plan.msg import FloatArrayMSG
 from baxter_plan.msg import GeomMSG
 from baxter_plan.msg import ParameterMSG
@@ -35,31 +36,62 @@ class PlanPublisher(object):
 		param_msg = ParameterMSG()
 		param_msg.type_name = param.get_type()
 		param_msg.is_symbol = param.is_symbol()
+
 		if hasattr(param, 'name') and type(param.name) is str:
 			param_msg.name = param.name
-		if hasattr(param, 'lArmPose') and type(param.lArmPose) is np.ndarray:
-			for joint in param.lArmPose:
-				param_msg.lArmPose.append(self.create_floatarray_msg(joint))
-		if hasattr(param, 'rArmPose') and type(param.rArmPose) is np.ndarray:
-			for joint in param.rArmPose:
-				param_msg.lArmPose.append(self.create_floatarray_msg(joint))
-		if hasattr(param, 'lGripper') and type(param.lGripper) is np.ndarray:
-			for grip in param.lGripper:
-				param_msg.lArmPose.append(self.create_floatarray_msg(grip))
-		if hasattr(param, 'rGripper') and type(param.rGripper) is np.ndarray:
-			for grip in param.rGripper:
-				param_msg.rGripper.append(self.create_floatarray_msg(grip))
-		if hasattr(param, 'pose') and type(param.pose) is np.ndarray:
-			for pose in param.pose:
-				param_msg.pose.append(self.create_floatarray_msg(pose))
-		if hasattr(param, 'value') and type(param.value) is np.ndarray:
-			for value in param.value:
-				param_msg.pose.append(self.create_floatarray_msg(value))
-		if hasattr(param, 'rotation') and type(param.rotation) is np.ndarray:
-			for rotation in param.rotation:
-				param_msg.pose.append(self.create_floatarray_msg(rotation))
+
+		if hasattr(param, 'lArmPose'):
+			if type(param.lArmPose) is np.ndarray:
+				for joint in param.lArmPose:
+					param_msg.lArmPose.append(self.create_floatarray_msg(joint))
+			else:
+				param_msg.undefined_attrs.append('lArmPose')
+
+		if hasattr(param, 'rArmPose'):
+			if type(param.rArmPose) is np.ndarray:
+				for joint in param.rArmPose:
+					param_msg.rArmPose.append(self.create_floatarray_msg(joint))
+			else:
+				param_msg.undefined_attrs.append('rArmPose')
+
+		if hasattr(param, 'lGripper'):
+			if type(param.lGripper) is np.ndarray:
+				for joint in param.lGripper:
+					param_msg.lGripper.append(self.create_floatarray_msg(joint))
+			else:
+				param_msg.undefined_attrs.append('lGripper')
+
+		if hasattr(param, 'rGripper'):
+			if type(param.rGripper) is np.ndarray:
+				for joint in param.rGripper:
+					param_msg.rGripper.append(self.create_floatarray_msg(joint))
+			else:
+				param_msg.undefined_attrs.append('rGripper')
+
+		if hasattr(param, 'pose'):
+			if type(param.pose) is np.ndarray:
+				for pose in param.pose:
+					param_msg.pose.append(self.create_floatarray_msg(pose))
+			else:
+				param_msg.undefined_attrs.append('pose')
+
+		if hasattr(param, 'value'):
+			if type(param.value) is np.ndarray:
+				for val in param.value:
+					param_msg.value.append(self.create_floatarray_msg(val))
+			else:
+				param_msg.undefined_attrs.append('value')
+
+		if hasattr(param, 'rotation'):
+			if type(param.rotation) is np.ndarray:
+				for rotation in param.rotation:
+					param_msg.rotation.append(self.create_floatarray_msg(rotation))
+			else:
+				param_msg.undefined_attrs.append('rotation')
+
 		if hasattr(param, 'geom'):
 			param_msg.geom = self.create_geom_msg(param.geom)
+
 		return param_msg
 
 	def create_predicate_msg(self, pred):
@@ -71,6 +103,14 @@ class PlanPublisher(object):
 			pred_msg.param_types.append(param.get_type())
 		return pred_msg
 
+	def create_actionpred_msg(self, actionpred):
+		actionpred_msg = ActionPredMSG()
+		actionpred_msg.negated = actionpred['negated']
+		actionpred_msg.hl_info = actionpred['hl_info']
+		actionpred_msg.pred = self.create_predicate_msg(actionpred['pred'])
+		actionpred_msg.active_timesteps = actionpred['active_timesteps']
+		return actionpred_msg
+
 	def create_action_msg(self, action):
 		action_msg = ActionMSG()
 		action_msg.name = action.name
@@ -78,7 +118,7 @@ class PlanPublisher(object):
 		for param in action.params:
 			action_msg.parameters.append(param.name)
 		for pred in action.preds:
-			action_msg.predicates.append(self.create_predicate_msg(pred['pred']))
+			action_msg.predicates.append(self.create_actionpred_msg(pred))
 		return action_msg
 
 	def create_plan_msg(self, plan):
