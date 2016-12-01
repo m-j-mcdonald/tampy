5b9184f7cc2423fa6880bae1c8fc66bdd87e2d14
Michael McDonald <m_j_mcdonald@comcast.net>
2016-11-30 23:23:52 -0800

    Switched to pickling

--- src/ros_interface/plan_publisher.py
+++ src/ros_interface/plan_publisher.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pickle
 
 import rospy
 from std_msgs.msg import String
@@ -14,102 +15,71 @@ from baxter_plan.msg import PredicateMSG
 class PlanPublisher(object):
 	def publish_plan(self, plan):
 		pub = rospy.Publisher('Plan', PlanMSG, queue_size=10)
-		pub2 = rospy.Publisher('test', ActionMSG, queue_size=10)
 		rospy.init_node('planner', anonymous=True)
 		msg = self.create_plan_msg(plan)
-		pub2.publish(self.create_action_msg(plan.actions[0]))
 		pub.publish(msg)
 
-	def create_floatarray_msg(self, array):
-		float_array_msg = FloatArrayMSG()
-		float_array_msg.data = array.tolist()
-		return float_array_msg
 
 	def create_geom_msg(self, geom):
 		geom_msg = GeomMSG()
-		geom_msg.type_name = str(type(geom)).split("'")[1].split(".")[-1]
-		attr_dict = {k:v for k,v in geom.__dict__.iteritems() if type(v) is float or type(v) is str}
-		geom_msg.attrs = str(attr_dict)[1:-1]
+		geom_msg.class_path = str(type(geom)).split("'")[1]
+		
+		try: 
+			geom_msg.data = pickle.dumps(geom)
+		except pickle.PicklingError:
+			print "Could not pickle {0}.".format(type(geom))
+			geom_msg.data = pickle.dumps(None)
+
 		return geom_msg
+
 
 	def create_parameter_msg(self, param):
 		param_msg = ParameterMSG()
-		param_msg.type_name = param.get_type()
-		param_msg.is_symbol = param.is_symbol()
 
-		if hasattr(param, 'name') and type(param.name) is str:
-			param_msg.name = param.name
+		param_msg.name = param.name
 
-		if hasattr(param, 'lArmPose'):
-			if type(param.lArmPose) is np.ndarray:
-				for joint in param.lArmPose:
-					param_msg.lArmPose.append(self.create_floatarray_msg(joint))
-			else:
-				param_msg.undefined_attrs.append('lArmPose')
-
-		if hasattr(param, 'rArmPose'):
-			if type(param.rArmPose) is np.ndarray:
-				for joint in param.rArmPose:
-					param_msg.rArmPose.append(self.create_floatarray_msg(joint))
-			else:
-				param_msg.undefined_attrs.append('rArmPose')
-
-		if hasattr(param, 'lGripper'):
-			if type(param.lGripper) is np.ndarray:
-				for joint in param.lGripper:
-					param_msg.lGripper.append(self.create_floatarray_msg(joint))
-			else:
-				param_msg.undefined_attrs.append('lGripper')
-
-		if hasattr(param, 'rGripper'):
-			if type(param.rGripper) is np.ndarray:
-				for joint in param.rGripper:
-					param_msg.rGripper.append(self.create_floatarray_msg(joint))
-			else:
-				param_msg.undefined_attrs.append('rGripper')
-
-		if hasattr(param, 'pose'):
-			if type(param.pose) is np.ndarray:
-				for pose in param.pose:
-					param_msg.pose.append(self.create_floatarray_msg(pose))
-			else:
-				param_msg.undefined_attrs.append('pose')
-
-		if hasattr(param, 'value'):
-			if type(param.value) is np.ndarray:
-				for val in param.value:
-					param_msg.value.append(self.create_floatarray_msg(val))
-			else:
-				param_msg.undefined_attrs.append('value')
-
-		if hasattr(param, 'rotation'):
-			if type(param.rotation) is np.ndarray:
-				for rotation in param.rotation:
-					param_msg.rotation.append(self.create_floatarray_msg(rotation))
-			else:
-				param_msg.undefined_attrs.append('rotation')
-
+		geom = None
 		if hasattr(param, 'geom'):
-			param_msg.geom = self.create_geom_msg(param.geom)
+			geom = param.geom
+			param.geom = None
+			param_msg.geom = self.create_geom_msg(geom)
+
+		if hasattr(param, 'openrave_body'):
+			param.openrave_body = None
+
+		try:
+			param_msg.data = pickle.dumps(param)
+		except pickle.PicklingError:
+			print "Could not pickle {0}.".format(param)
+
+		if geom:
+			param.geom = geom
 
 		return param_msg
 
+
 	def create_predicate_msg(self, pred):
 		pred_msg = PredicateMSG()
-		pred_msg.type_name = pred.get_type()
+		pred_msg.class_path = str(type(pred)).split("'")[1]
+		params = pred.params
+		env = pred.env
+
 		pred_msg.name = pred.name
 		for param in pred.params:
-			pred_msg.params.append(self.create_parameter_msg(param))
+			pred_msg.parameters.append(self.create_parameter_msg(param))
 			pred_msg.param_types.append(param.get_type())
+
 		return pred_msg
+
 
 	def create_actionpred_msg(self, actionpred):
 		actionpred_msg = ActionPredMSG()
 		actionpred_msg.negated = actionpred['negated']
 		actionpred_msg.hl_info = actionpred['hl_info']
-		actionpred_msg.pred = self.create_predicate_msg(actionpred['pred'])
+		actionpred_msg.predicate = self.create_predicate_msg(actionpred['pred'])
 		actionpred_msg.active_timesteps = actionpred['active_timesteps']
 		return actionpred_msg
+
 
 	def create_action_msg(self, action):
 		action_msg = ActionMSG()
@@ -121,6 +91,7 @@ class PlanPublisher(object):
 			action_msg.predicates.append(self.create_actionpred_msg(pred))
 		return action_msg
 
+
 	def create_plan_msg(self, plan):
 		plan_msg = PlanMSG()
 		plan_msg.horizon = plan.horizon
@@ -130,6 +101,7 @@ class PlanPublisher(object):
 			plan_msg.actions.append(self.create_action_msg(action))
 
 		return plan_msg
+
 
 if __name__ == "__main__":
 	pb = PlanPublisher()
--- src/ros_interface/plan_receiver.py
+++ src/ros_interface/plan_receiver.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
 import argparse
+import importlib
+import json
+import pickle
 import sys
 import threading
 import Queue
@@ -17,24 +20,15 @@ import baxter_interface
 
 from baxter_interface import CHECK_VERSION
 
+from openravepy import Environment
+
 from core.internal_repr.action import Action
 from core.internal_repr.parameter import Object, Symbol
 from core.internal_repr.plan import Plan
-from core.internal_repr.predicate import Predicate
-from core.util_classes.baxter_predicates import *
-from core.util_classes.box import Box
-from core.util_classes.can import Can, BlueCan, RedCan, GreenCan
-from core.util_classes.circle import Circle, BlueCircle, RedCircle, GreenCircle
-from core.util_classes.common_predicates import *
-from core.util_classes.matrix import *
 from core.util_classes.robots import Baxter
-from core.util_classes.robot_predicates import *
-from core.util_classes.table import Table
-from core.util_classes.wall import Wall
-
-from baxter_plan.msg import ActionMSG, FloatArrayMSG, GeomMSG, ParameterMSG, PlanMSG, PredicateMSG
 
 from baxter_plan import execute_action
+from baxter_plan.msg import PlanMSG
 
 class PlanReceiver(object):
 	def listen_for_plans(self):
@@ -83,249 +77,57 @@ class PlanReceiver(object):
 		actionpred = {}
 		actionpred['negated'] = data.negated
 		actionpred['hl_info'] = data.hl_info
-		actionpred['pred'] = self._build_pred(data.pred, plan_params, env)
+		actionpred['pred'] = self._build_pred(data.predicate, plan_params, env)
 		actionpred['active_timesteps'] = (data.active_timesteps[0], data.active_timesteps[1])
 		return actionpred
 
 
 	def _build_pred(self, data, plan_params, env):
-		pred_class = eval(data.type_name)
+		class_path = data.class_path.rsplit(".", 1)
+		pred_module = importlib.import_module(class_path[0])
+		pred_class = getattr(pred_module, class_path[1])
+
 		params = []
-		for param in data.params:
+		for param in data.parameters:
 			if param.name in plan_params.keys():
 				params.append(plan_params[param.name])
 			else:
 				params.append(self._build_param(param))
+
 		return pred_class(data.name, params, data.param_types, env)
 
 
 	def _build_param(self, data):
-		attr_types = {}
+		try:
+			param = pickle.loads(data.data)
+		except pickle.UnpicklingError:
+			print "Could not unpickle parameter."
+			raise
 
-		attr_types['_type'] = str
-		attr_types['name'] = str
+		if data.geom.class_path != "":
+			param.geom = self._build_geom(data.geom)
 
-		if data.geom.type_name != '':
-			attr_types['geom'] = eval(data.geom.type_name)
-
-		if data.type_name == 'Robot':
-			attr_types['pose'] = Vector1d
-			attr_types['lArmPose'] = ArmPose7d
-			attr_types['lGripper'] = Vector1d
-			attr_types['rArmPose'] = ArmPose7d
-			attr_types['rGripper'] = Vector1d
-		elif data.type_name == "RobotPose":
-			attr_types['value'] = Vector1d
-			attr_types['lArmPose'] = ArmPose7d
-			attr_types['lGripper'] = Vector1d
-			attr_types['rArmPose'] = ArmPose7d
-			attr_types['rGripper'] = Vector1d
-		elif data.type_name == 'Can':
-			attr_types['pose'] = Vector3d
-			attr_types['rotation'] = Vector3d
-		elif data.type_name == 'Obstacle':
-			attr_types['pose'] = Vector3d
-			attr_types['rotation'] = Vector3d
-		elif data.type_name == 'EEPose':
-			attr_types['value'] = Vector3d
-			attr_types['rotation'] = Vector3d
-		elif data.type_name == 'Target':
-			attr_types['value'] = Vector3d
-			attr_types['rotation'] = Vector3d
-		else:
-			raise Exception("Missing {0} in _build_param.".format(data.type_name))
-
-		if data.is_symbol:
-			new_param = Symbol(attr_types=attr_types)
-		else:
-			new_param = Object(attr_types=attr_types)
-
-		new_param._type = data.type_name
-		new_param.name = data.name
-
-		if data.geom.type_name != '':
-			new_param.geom = self._build_geom(data.geom)
-
-		if data.type_name == 'Robot':
-			new_param.lArmPose = self._build_2D_numpy_array(data.lArmPose)
-			new_param.lGripper = self._build_2D_numpy_array(data.lGripper)
-			new_param.rArmPose = self._build_2D_numpy_array(data.rArmPose)
-			new_param.rGripper = self._build_2D_numpy_array(data.rGripper)
-			new_param.pose = self._build_2D_numpy_array(data.pose)
-		elif data.type_name == "RobotPose":
-			new_param.lArmPose = self._build_2D_numpy_array(data.lArmPose)
-			new_param.lGripper = self._build_2D_numpy_array(data.lGripper)
-			new_param.rArmPose = self._build_2D_numpy_array(data.rArmPose)
-			new_param.rGripper = self._build_2D_numpy_array(data.rGripper)
-			new_param.value = self._build_2D_numpy_array(data.value)
-		elif data.type_name == 'Can':
-			new_param.rotation = self._build_2D_numpy_array(data.rotation)
-			new_param.pose = self._build_2D_numpy_array(data.pose)
-		elif data.type_name == 'Obstacle':
-			new_param.rotation = self._build_2D_numpy_array(data.rotation)
-			new_param.pose = self._build_2D_numpy_array(data.pose)
-		elif data.type_name == 'EEPose':
-			new_param.rotation = self._build_2D_numpy_array(data.rotation)
-			new_param.value = self._build_2D_numpy_array(data.value)
-		elif data.type_name == 'Target':
-			new_param.rotation = self._build_2D_numpy_array(data.rotation)
-			new_param.value = self._build_2D_numpy_array(data.value)
-
-		for attr in data.undefined_attrs:
-			setattr(new_param, attr, 'undefined')
-
-		return new_param
+		return param
 
 
 	def _build_geom(self, data):
-		geom_class = eval(data.type_name)
-		attrs = {}
-		for attr in data.attrs.split(", "):
-			attr = attr.split(": ")
-			attrs[attr[0]] = attr[1]
+		class_path = data.class_path.rsplit(".", 1)
+		geom_module = importlib.import_module(class_path[0])
+		geom_class = getattr(geom_module, class_path[1])
+		if data.data:
+			try:
+				geom = pickle.loads(data.data)
+			except pickle.UnpicklingError:
+				print "Cannot unpickle geometry {0}.".format(geom_class)
+		else:
+			geom = geom_class()
 
-		if issubclass(geom_class, Can):
-			radius = float(attrs["'radius'"])
-			height = float(attrs["'height'"])
-			return geom_class(radius, height)
-		elif geom_class is Baxter:
-			geometry = geom_class()
+		if geom_class is Baxter:
 			# Fix this
-			geometry.shape = "/home/michael/robot_work/tampy/models/baxter/baxter.xml"
-			return geometry
-		elif geom_class is Box:
-			dim = [float(attrs["'length'"]), float(attrs["'height'"]), float(attrs["'width'"])]
-			return geom_class(dim)
-		elif geom_class is Table:
-			raise Exception("Yeah, fix this. It's in _build_geom of plan_receiver.")
-		elif issubclass(geom_class, Circle):
-			radius = float(attrs["'radius'"])
-			return geom_class(radius)
-		elif geom_class is Wall:
-			wall_type = attrs["'wall_type'"]
-			return geom_class(wall_type)
+			geom.shape = "/home/michael/robot_work/tampy/models/baxter/baxter.xml"
+				
+		return geom
 
-		raise Exception('Geometry {0} not implemented yet.'.format(data.type_name))
-
-
-	def _build_2D_numpy_array(self, float_array_msgs):
-		new_array = []
-		for row in float_array_msgs:
-			new_array.append(row.data)
-		return np.array(new_array)
-
-
-	def _execute_action(self, action):
-		def get_joint_positions(limb, pos, i):
-			return {limb + "_s0": pos[0][i], limb + "_s1": pos[1][i], limb + "_e0": pos[2][i], limb + "_e1": pos[3][i], limb + "_w0": pos[4][i], limb + "_w1": pos[5][i], limb + "_w2": pos[6][i]}
-
-		baxter = None
-		for param in action.params:
-			if param.name == 'baxter':
-				baxter = param
-
-		if not baxter:
-			raise Exception("Baxter not found for action: %s" % action.name)
-
-		l_arm_pos = baxter.lArmPose
-		l_gripper = baxter.lGripper[0]
-		r_arm_pos = baxter.rArmPose
-		r_gripper = baxter.rGripper[0]
-
-		print("Getting robot state... ")
-		rs = baxter_interface.RobotEnable(CHECK_VERSION)
-		init_state = rs.state().enabled
-
-		def clean_shutdown():
-			print("\nExiting example...")
-			if not init_state:
-				print("Disabling robot...")
-				rs.disable()
-		rospy.on_shutdown(clean_shutdown)
-
-		print("Enabling robot... ")
-		rs.enable()
-		print("Running. Ctrl-c to quit")
-
-		left = baxter_interface.limb.Limb("left")
-		right = baxter_interface.limb.Limb("right")
-		grip_left = baxter_interface.Gripper('left', CHECK_VERSION)
-		grip_right = baxter_interface.Gripper('right', CHECK_VERSION)
-
-		left_queue = Queue.Queue()
-		right_queue = Queue.Queue()
-		rate = rospy.Rate(1000)
-
-		if grip_left.error():
-			grip_left.reset()
-		if grip_right.error():
-			grip_right.reset()
-		if (not grip_left.calibrated() and
-			grip_left.type() != 'custom'):
-			grip_left.calibrate()
-		if (not grip_right.calibrated() and
-			grip_right.type() != 'custom'):
-			grip_right.calibrate()
-
-		left.move_to_joint_positions(get_joint_positions("left", l_arm_pos, 0))
-		right.move_to_joint_positions(get_joint_positions("right", r_arm_pos, 0))
-
-
-		def move_thread(limb, gripper, angle, grip, queue, timeout=15.0):
-				"""
-				Threaded joint movement allowing for simultaneous joint moves.
-		        """
-				try:
-					limb.move_to_joint_positions(angle, timeout)
-					gripper.command_position(grip)
-					queue.put(None)
-				except Exception, exception:
-					queue.put(traceback.format_exc())
-					queue.put(exception)
-
-		for i in range(0, len(l_gripper)):
-
-			left_thread = threading.Thread(
-				target=move_thread,
-				args=(left,
-					grip_left,
-					get_joint_positions("left", l_arm_pos, i),
-					l_gripper[i],
-					left_queue
-					)
-			)
-			right_thread = threading.Thread(
-				target=move_thread,
-				args=(right,
-				grip_right,
-				get_joint_positions("right", r_arm_pos, i),
-				r_gripper[i],
-				right_queue
-				)
-			)
-
-			left_thread.daemon = True
-			right_thread.daemon = True
-			left_thread.start()
-			right_thread.start()
-			baxter_dataflow.wait_for(
-				lambda: not (left_thread.is_alive() or right_thread.is_alive()),
-				timeout=20.0,
-				timeout_msg=("Timeout while waiting for arm move threads to finish"),
-				rate=10,
-			)
-			left_thread.join()
-			right_thread.join()
-			result = left_queue.get()
-			if not result is None:
-				raise left_queue.get()
-			result = right_queue.get()
-			if not result is None:
-				raise right_queue.get()
-
-def receive_plan():
-	pr = PlanReceiver()
-	pr.listen_for_plans()
 
 if __name__ == "__main__":
-	receive_plan()
+	PlanReceiver().listen_for_plans()
